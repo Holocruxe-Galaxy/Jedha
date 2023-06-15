928a95d258144e05626c8cc10a3f4beaca907add
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-06-15 13:11:14 -0300

    improving search path, and status code

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,58 @@
-import { Controller, Get, Param, Put } from '@nestjs/common';
+import {
+  Controller,
+  Res,
+  Get,
+  Param,
+  Put,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Get()
-  getUsers() {
-    const users = this.userService.getUsers();
+  async getUsers() {
+    const users = await this.userService.getUsers();
+    if (users['status']) {
+      throw new HttpException(users['message'], users['status']);
+    }
     return users;
   }
 
   @Get('/:id')
-  getUserById(@Param('id') id: string) {
-    const user = this.userService.getUserById(id);
+  async getUserById(@Param('id') id: string) {
+    const user = await this.userService.getUserById(id);
+    if (user['status']) {
+      throw new HttpException(user['message'], user['status']);
+    }
+
     return user;
   }
 
   @Get('/search/:name')
   async findUsersByName(@Param('name') name: string) {
+    if (name === ' ') {
+      throw new HttpException(
+        'it is necessary to enter at least one letter to be able to search',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
     const users = await this.userService.findUserByname(name);
+    if (users['status']) {
+      throw new HttpException(users['message'], users['status']);
+    }
     return users;
   }
   @Put('/ban/:id')
-  banUser(@Param('id') id: string) {
-    return this.userService.userBan(id);
+  async banUser(@Param('id') id: string) {
+    const response = await this.userService.userBan(id);
+    if (response['status']) {
+      throw new HttpException(response['message'], response['status']);
+    }
+    return response;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ValidationPipe,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -9,42 +14,121 @@ export class UsersService {
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
 
-  getUsers(): Promise<User[]> {
-    return this.usersRepository.find();
+  async getUsers() {
+    try {
+      const users = await this.usersRepository.find({
+        select: [
+          'id',
+          'name',
+          'username',
+          'lastname',
+          'image_profile_url',
+          'email',
+          'ban',
+          'premium',
+        ],
+      });
+
+      if (!users.length) {
+        return new HttpException(
+          'there are currently no users in the database',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return users;
+    } catch (error) {
+      return new HttpException(error.message, 500);
+    }
   }
 
-  async getUserById(id: string): Promise<User> {
-    const user = await this.usersRepository.findOne({ where: { id: id } });
-    return user;
+  async getUserById(id: string) {
+    try {
+      const user = await this.usersRepository.findOne({ where: { id: id } });
+      if (!user) {
+        return new HttpException('user not found', HttpStatus.NOT_FOUND);
+      }
+      return user;
+    } catch (error) {
+      return new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
   async findUserByname(
     // username: string,
     name: string,
     // lastname: string,
-  ): Promise<User[]> {
-    const allUsers = await this.usersRepository.find({
-      select: [
-        'id',
-        'name',
-        'username',
-        'lastname',
-        'image_profile_url',
-        'email',
-      ],
-    });
-    const usersFilter = allUsers.filter(
-      (e) =>
-        e.name.toUpperCase().includes(name.toUpperCase()) ||
-        e.lastname.toUpperCase().includes(name.toUpperCase()) ||
-        e.username.toUpperCase().includes(name.toUpperCase()),
-    );
-
-    return usersFilter;
+  ) {
+    try {
+      if (!name.length) {
+        throw new HttpException(
+          'it is necessary to enter at least one letter to be able to search',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      const allUsers = await this.usersRepository.find({
+        select: [
+          'id',
+          'name',
+          'username',
+          'lastname',
+          'image_profile_url',
+          'email',
+          'ban',
+          'premium',
+        ],
+      });
+      function removeTildes(texto: string): string {
+        const tildes = {
+          á: 'a',
+          é: 'e',
+          í: 'i',
+          ó: 'o',
+          ú: 'u',
+          Á: 'A',
+          É: 'E',
+          Í: 'I',
+          Ó: 'O',
+          Ú: 'U',
+        };
+        return texto.replace(/[áéíóúÁÉÍÓÚ]/g, (letra) => tildes[letra]);
+      }
+      const usersFilter = allUsers.filter((e) =>
+        [
+          removeTildes(e.username.toUpperCase()),
+          removeTildes(e.name.toUpperCase()),
+          removeTildes(e.lastname.toUpperCase()),
+        ]
+          .join(' ')
+          .includes(removeTildes(name.toUpperCase())),
+      );
+      if (!usersFilter.length) {
+        return new HttpException('no match found', HttpStatus.NOT_FOUND);
+      }
+      return usersFilter;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async userBan(id: string) {
-    const user = await this.usersRepository.findOne({ where: { id: id } });
-    const userBan = this.usersRepository.save({ ...user, ban: !user.ban });
-    return userBan;
+    try {
+      const user = await this.usersRepository.findOne({ where: { id: id } });
+      if (!user) {
+        return new HttpException(
+          'the user was not found',
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      const userBan = await this.usersRepository.save({
+        ...user,
+        ban: !user.ban,
+      });
+      if (!userBan.ban) {
+        return 'the user was successfully unbanned';
+      } else {
+        return 'the user was successfully banned';
+      }
+    } catch (error) {
+      return new HttpException(error, 400);
+    }
   }
 }
